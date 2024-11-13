03eef9592d1c466dd983a39cb140a47a261bdd7a
nekedos <nekedos@gmail.com>
2024-11-13 15:45:12 +0400

    chore(demo): set up the project

---
+++ README.md
@@ -0,0 +1,20 @@
+# Tonstakers Demo
+
+## Installation
+
+```bash
+corepack enable
+yarn
+```
+
+## Development
+
+```bash
+yarn dev
+```
+
+## Production
+
+```bash
+yarn build
+```
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react-swc';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  return {
+    plugins: [
+      typescriptPaths({
+        preserveExtensions: true,
+        tsConfigPath: './tsconfig.json',
+      }),
+      react(),
+    ],
+  };
+});
